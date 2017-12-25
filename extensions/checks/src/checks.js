4759542ee8ea418b8309c9faaaa3667284346e28
Ryan Northey <ryan@synca.io>
2017-12-25 22:11:44 +0000

    Add checks l10n addon

---
+++ extensions/checks/src/checks.js
@@ -0,0 +1,49 @@
+
+import Plugin from 'l10n-tools/plugins/addon/plugin';
+
+
+class ChecksRunner {
+    get checks () {
+        return {
+            'End punctuation': ["(?<endpunc>[.!?])$", "(?<endpunc>[.!?])$"],
+            'No script tags': ["", '<script']
+        };
+    }
+}
+
+
+class ChecksPlugin extends Plugin {
+
+    constructor() {
+        super()
+        this.icon = 'images/checks.svg';
+        this.provides = ['l10n.tools.checks'];
+        this.runner = new ChecksRunner();
+        this.tools = {
+            'l10n.tools.checks': {
+                checksCapitalizationMatches: {
+                    category: "toolsChecksBasic",
+                    name: "checksCapitalizationMatches",
+                    sourceRegex: '^[A-Z]',
+                    targetRegex: '^[A-Z]',
+                    defaultEnabled: true}}}
+    }
+
+    get checks () {
+        return this.runner.checks;
+    }
+
+    handle(request, sender, sendResponse) {
+        if (request.message === 'runChecks') {
+            sendResponse(
+                this.runner.runChecks(
+                    request.data.entity,
+                    request.data.submitted,
+                    request.data.submitted_plural));
+            return true;
+        }
+    }
+}
+
+let checks = new ChecksPlugin();
+checks.setup();
