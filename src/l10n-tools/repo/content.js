1f1f16af14e6ddedeb17c78acdeace5ff110f08d
Ryan Northey <ryan@synca.io>
2017-12-26 15:34:50 +0000

    Add base l10n repo classes

---
+++ src/l10n-tools/repo/content.js
@@ -0,0 +1,11 @@
+
+import WebContent from 'l10n-tools/plugins/content/web';
+
+
+export default class WebRepoContent extends WebContent {
+
+    constructor () {
+	super()
+	this.matchRepo();
+    }
+}
