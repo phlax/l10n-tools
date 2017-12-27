7e038cf92278d2ea86d71859a9cbadd02a2c8829
Ryan Northey <ryan@synca.io>
2017-12-27 19:19:16 +0000

    Add weblate l10n addon

---
+++ extensions/weblate/src/weblate-app.js
@@ -0,0 +1,55 @@
+
+import L10nWebApp from 'l10n-tools/app/web';
+
+
+export default class WeblateApp extends L10nWebApp {
+
+    constructor() {
+        super();
+        this.translateMessage = 'Translate this file on Weblate';
+    }
+
+    getEndpoint() {
+        return this.app.url + '/graphql?query={projects{slug,name,deadline,priority,repositories{url,website},localizations{locale{code,name}}}}';
+    }
+
+    getHrefFromRepoPath(project, filePath) {
+        const parts = filePath.substring('locales/'.length).split('/');
+        return this.app.url + '/' + parts[0] + '/' + project.slug + '/' + parts[1];
+    }
+
+    parseProjects(blob) {
+        return blob.data.projects;
+    }
+
+    matchRepo(url) {
+        let projects = Array.from(this.app.projects);
+        for (let p in projects) {
+            for (let r in projects[p].repositories) {
+                let repo = projects[p].repositories[r];
+                let webRepo = repo.website + '/blob/master/';
+                if (url.startsWith(webRepo)) {
+                    let filePath = url.substring(webRepo.length);
+                    if (filePath.startsWith('locales/')) {
+                        return this.repoMatch(projects[p], filePath)
+                    }
+                }
+            }
+        }
+    }
+
+    handle(request) {
+        if (request.message === 'fetchProjects') {
+            return this.fetchProjects()
+        }
+        if (request.message === 'matchRepo') {
+            let promise = new Promise(resolve => {
+                let result = this.matchRepo(request.url);
+                if (result) {
+                    resolve(result);
+                }
+            })
+            return promise;
+        }
+    }
+}
---
+++ extensions/weblate/src/weblate.js
@@ -0,0 +1,32 @@
+
+import WeblateApp from "./weblate-app"
+import AppPlugin from 'l10n-tools/plugins/addon/app';
+
+const longDescription = [
+    'Provides the following features',
+    '- Custom checks in Weblate',
+    '- Custom MT/TM services in Weblate',
+    '- Hookup with upstream respositories (if relevant addon installed)',
+    '- In context translation (?!??)',
+    '',
+].join('\n');
+
+
+class WeblatePlugin extends AppPlugin {
+
+    constructor() {
+        super()
+        this.name = 'Weblate'
+        this.icon = 'images/weblate.svg'
+        this.provides = ['l10n.apps.web']
+        this.app = WeblateApp;
+        this.longDescription = longDescription;
+        this.recommendedApps = [
+            {name: "Weblate",
+             icon: "/images/weblate.svg",
+             url: "http://localhost:8000"}];
+        }
+}
+
+let weblate = new WeblatePlugin();
+weblate.setup();
