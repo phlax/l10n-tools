0fb53c1b2836ced435c7feeab033a005ab844f62
Ryan Northey <ryan@synca.io>
2018-01-14 19:44:46 +0000

    Add terminology service

---
+++ src/l10n-tools/manager/config/services/terminology/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+export default class TerminologyServicesConfig extends React.Component {
+
+    render() {
+        const {terminologys} = this.state;
+        return (
+            <div>
+                List of terminology services registered...
+            </div>);
+    }
+}
