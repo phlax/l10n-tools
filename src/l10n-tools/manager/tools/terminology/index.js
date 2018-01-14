9faee1c0162d5637984fc8cdfb1fc6b5cffff856
Ryan Northey <ryan@synca.io>
2018-01-14 22:50:49 +0000

    Add terminology registries to term tool

--- src/l10n-tools/manager/tools/terminology/index.js
+++ src/l10n-tools/manager/tools/terminology/index.js
@@ -3,8 +3,17 @@ import TerminologyToolConfig from 'l10n-tools/manager/config/tools/terminology';
 import PluginExtension from 'web-ext-plugins/manager/extension';
 import schema from 'l10n-tools/schema/tools/terminology.json'
 
+import {TerminologyRegistry, TerminologyFileRegistry} from './registry';
+
 
 export default class TerminologyTool extends PluginExtension {
+
+
+    constructor (manager) {
+        super(manager)
+        this.files = new TerminologyFileRegistry(this);
+        this.terminology = new TerminologyRegistry(this);
+    }
 
     get name () {
         return "toolsTerminologyName";
@@ -16,5 +25,15 @@ export default class TerminologyTool extends PluginExtension {
 
     get configComponent () {
         return TerminologyToolConfig
+    }
+
+    importData (filename, data, lastModified) {
+        return this.files.register({filename: filename, lastModified: lastModified}).then(() => {
+            return this.files.get().then(() => {
+                data.map(d => d.filename = filename)
+                data = data.filter(d => (d.filename && d.term && d.translation))
+                return this.terminology.register([...data])
+            })
+        })
     }
 }
