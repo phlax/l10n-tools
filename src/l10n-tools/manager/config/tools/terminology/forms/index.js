db16bc416321080771c46980eab673a8de735afc
Ryan Northey <ryan@synca.io>
2018-01-14 13:37:41 +0000

    Adjust terminology upload form

--- src/l10n-tools/manager/config/tools/terminology/forms/index.js
+++ src/l10n-tools/manager/config/tools/terminology/forms/index.js
@@ -1,39 +1,19 @@
 
 import React from 'react';
 
-import Fieldset from 'web-ext-plugins/widgets/fieldset';
 import TerminologyImportTable from './import';
 import TBXParser from 'l10n-tools/formats/tbx/parser';
+import {ParsedUploadForm} from 'web-ext-plugins/widgets/upload';
 
 
-export default class CustomTerminologyForm extends React.Component {
+export default class CustomTerminologyForm extends ParsedUploadForm {
 
-    constructor (props) {
-        super(props);
-        this.state = {termEntries: []};
-        this.parser = new TBXParser();
-        this.uploadFile = this.uploadFile.bind(this);
+    get parser () {
+        return new TBXParser();
     }
 
-    uploadFile (evt) {
-        const curFiles = evt.target.files;
-        if (curFiles.length > 0) {
-            this.parser.parseFile(curFiles[0], entries => {
-                this.setState({termEntries: entries});
-            });
-        }
+    get tableComponent () {
+        return TerminologyImportTable;
     }
 
-    render() {
-        const {termEntries} = this.state;
-        return (
-            <form>
-              <Fieldset legend="">
-                <input type="file" onChange={this.uploadFile} />
-              </Fieldset>
-              {termEntries.length > 0 &&
-                  <TerminologyImportTable data={termEntries} />
-              }
-            </form>);
-    }
 }
