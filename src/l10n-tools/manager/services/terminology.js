a385766bc0509b59faf99c738b5ae95ae73a57fe
Ryan Northey <ryan@synca.io>
2018-01-14 12:24:52 +0000

    Add terminology service

---
+++ src/l10n-tools/manager/services/terminology.js
@@ -0,0 +1,20 @@
+
+import PluginExtension from 'web-ext-plugins/manager/extension';
+import TerminologyServiceConfig from 'l10n-tools/manager/config/services/terminology';
+import schema from 'l10n-tools/schema/terminology.json';
+
+
+export default class TerminologyService extends PluginExtension {
+
+    get name () {
+        return "servicesTerminologyName";
+    }
+
+    get schema () {
+        return schema;
+    }
+
+    get configComponent () {
+        return TerminologyServiceConfig;
+    }
+}
